28fcc7d288e118ffeb7f7fc5443ee685dd0bddc1
Lee Jae Won <varyly@naver.com>
2017-10-08 02:15:48 +0900

    before test src/utils.c

--- src/utils.c
+++ src/utils.c
@@ -7,9 +7,9 @@ void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
-  char** ar=(char**)malloc(sizeof(char*));
-  int count=0;
-  char* token;
+ char** ar=(char**)malloc(sizeof(char*));
+ int count=0;
+ char* token;
 char seps[]=" ,\t\n";
 char* cmd=(char*)malloc(sizeof(char));
 strcpy(cmd,command)
