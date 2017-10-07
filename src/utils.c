3cb34acb8f6f6749e11a2644ac4a1504d981b8b9
Lee Jae Won <varyly@naver.com>
2017-10-08 01:55:28 +0900

    test

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,12 @@
 #include "utils.h"
+#include <stdlib.h>
+#include <string.h>
+
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
+  char** ar=(char**)malloc(sizeof(char*));
+  int count;
 }
