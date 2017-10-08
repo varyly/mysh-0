f0a7d26a92fb7f0319fd97009d4a11efe2eb114f
Lee Jae Won <varyly@naver.com>
2017-10-08 15:09:31 +0900

    utils.c has error

--- src/utils.c
+++ src/utils.c
@@ -1,27 +1,28 @@
 #include "utils.h"
 #include <stdlib.h>
 #include <string.h>
-
+#define MAXSIZE 100
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
   // TODO: Fill this!
- char** ar=(char**)malloc(sizeof(char*));
+ *argv=(char**)malloc(sizeof(char*)*MAXSIZE);
  int count=0;
  char* token;
 char seps[]=" ,\t\n";
-char* cmd=(char*)malloc(sizeof(char));
+char* cmd=(char*)malloc(sizeof(char)*MAXSIZE);
 strcpy(cmd,command);
 token=strtok(cmd,seps);
+
 while(token!=NULL)
 {
-ar[count]=(char*)malloc(sizeof(char));
-strcpy(ar[count],token);
+(*argv)[count]=(char*)malloc(sizeof(char));
+strcpy(*argv[count],token);
 token=strtok(NULL,seps);
 count++;
 }
 *argc=count;
-*argv=ar;
+
 
 }
